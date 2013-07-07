06fd141bcc7543c87a81fc42afb2817cb4b34a77
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2013-07-06 22:25:52 -0300

    Fix the lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class postbox {
   package { 'postbox':
     provider => 'appdmg',
-    source => 'http://download.getpostbox.com.s3.amazonaws.com/installers/3.0.7/1_45019249cef6c9df058a92141b150f471334aab2/postbox-3.0.7-mac64.dmg'
+    source   => 'http://download.getpostbox.com.s3.amazonaws.com/installers/3.0.7/1_45019249cef6c9df058a92141b150f471334aab2/postbox-3.0.7-mac64.dmg'
   }
 }
